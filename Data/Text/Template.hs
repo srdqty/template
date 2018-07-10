515ca547698950ca7dbd9e1f6bbb5cc71d02c865
Sofia Donataccio <srdqty@gmail.com>
2018-07-10 12:17:00 -0400

    Allows names to be all uppercase

--- Data/Text/Template.hs
+++ Data/Text/Template.hs
@@ -50,7 +50,7 @@ module Data.Text.Template
 import Control.Applicative (Applicative(pure), (<$>))
 import Control.Monad (liftM, liftM2, replicateM_)
 import Control.Monad.State.Strict (State, evalState, get, put)
-import Data.Char (isAlphaNum, isLower)
+import Data.Char (isAlphaNum)
 import Data.Function (on)
 import Data.Maybe (fromJust, isJust)
 import Data.Monoid (Monoid(mempty, mappend))
@@ -296,7 +296,7 @@ pLit = do
     return $ Lit s
 
 isIdentifier0 :: Char -> Bool
-isIdentifier0 c = or [isLower c, c == '_']
+isIdentifier0 c = or [isAlphaNum c, c == '_']
 
 isIdentifier1 :: Char -> Bool
 isIdentifier1 c = or [isAlphaNum c, c `elem` "_'"]
